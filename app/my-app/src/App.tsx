5378acaebd4c6891a44adb662657cfdee0908dbe
DanilKoshnarev <146003208+DanilKoshnarev@users.noreply.github.com>
2024-11-20 15:07:12 +0200

    Add files via upload
    Added a homework

---
+++ app/my-app/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App: React.FunctionComponent = () => {
+  return (
+    <div>
+      <h1>Хаю хай!</h1>
+      <p>Это мой мейнстрим.</p>
+      <img src="app/catimage.jpg" alt="Милашка:)" />
+    </div>
+  );
+};
+
+export default App;
